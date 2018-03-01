3f835eeac48589a82a13e2a68923dc2f56ba40eb
yanghao <yanghao@wtoip.com>
2018-03-01 11:58:00 +0800

    'first'

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+         <div className="row">
+            <div className="col-xs-1 col-xs-offset-11"></div>
+            <h1>hello</h1>
+         </div>
+      </div>
+    );
+  }
+}
+
+export default App;
